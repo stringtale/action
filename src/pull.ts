964a1a0b0cf9de1e902ca06f54a583a076e3a3d1
René Vlugt <rene@demonsters.nl>
2024-10-29 16:26:21 +0100

    Update @stringtale/utils to remove node-fetch requirement

--- src/pull.ts
+++ src/pull.ts
@@ -1,51 +1,44 @@
-
-
 // import replace from "replace-in-file"
-import { glob } from "glob"
-import fs from "fs"
-import path from "path"
-import { replaceInFile } from "@stringtale/utils"
-import { pull } from "@stringtale/node"
-import * as core from "@actions/core"
+import { glob } from "glob";
+import fs from "fs";
+import path from "path";
+import { replaceInFile } from "@stringtale/utils";
+import { pull } from "@stringtale/node";
+import * as core from "@actions/core";
 
+const pullAndReplace = async ({
+  token,
+  files = ["**/*.tsx", "**/*.ts", "**/*.jsx", "**/*.js"],
+  root,
+  ignore = [],
+}: any) => {
+  core.info("Fetch values from StringTale");
 
-const pullAndReplace = async ({ token, files = ["**/*.tsx", "**/*.ts", "**/*.jsx", "**/*.js"], root, ignore = [] }: any) => {
+  const data = await pull(token);
 
-  core.info("Fetch values from StringTale")
-
-  const nodeFetch = await import("node-fetch")
-  // @ts-ignore
-  global.fetch = nodeFetch.default
-  // @ts-ignore
-  global.Headers = nodeFetch.Headers
-  
-  const data = await pull(token)
-
-  core.info("Get files to update: " + files.toString())
+  core.info("Get files to update: " + files.toString());
 
   const filesPaths = await glob(files, {
     ignore,
     cwd: root,
-    nodir: true
-  })
-  
-  let filesChanged: string[] = []
+    nodir: true,
+  });
+
+  let filesChanged: string[] = [];
 
   for (const file of filesPaths) {
+    const fullPath = root ? path.resolve(root, file) : file;
 
-    const fullPath = root ? path.resolve(root, file) : file
-
-    const input = fs.readFileSync(fullPath, 'utf-8')
-    const output = replaceInFile(input, data.content, file)
+    const input = fs.readFileSync(fullPath, "utf-8");
+    const output = replaceInFile(input, data.content, file);
     //Contents changed and not a dry run? Write to file
     if (output !== input) {
-      fs.writeFileSync(file, output, 'utf8')
-      core.info("Writen file: " + fullPath)
-      filesChanged.push(file)
+      fs.writeFileSync(file, output, "utf8");
+      core.info("Writen file: " + fullPath);
+      filesChanged.push(file);
     }
   }
-  return filesChanged
+  return filesChanged;
+};
 
-}
-
-export default pullAndReplace
+export default pullAndReplace;
